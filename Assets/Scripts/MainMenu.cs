1bd6f2358a4df65e135e9ca3d5c5ebced3ffbae0
Hitori <codename.virtualentity14321@gmail.com>
2023-07-07 16:04:33 +0700

    Finish Demo

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,16 +6,23 @@ using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private Button StartGameButton;
+    [SerializeField] private Button QuitButton;
     [SerializeField] private GameManager gameManager;
 
     private void Awake()
     {
         StartGameButton.onClick.AddListener(OnStartGameButtonClick);
+        QuitButton.onClick.AddListener(OnQuitButtonClick);
     }
 
     private void OnStartGameButtonClick()
     {
         gameManager.StartGame();
         gameObject.SetActive(false);
+    }
+
+    private void OnQuitButtonClick()
+    {
+        gameManager.Quit();
     }
 }
